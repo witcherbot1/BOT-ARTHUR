02658f910af30f1882bcd727e1f2fc686c945476
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-14 18:45:43 +0300

    Update بوت.js

--- plugins/بوت.js
+++ plugins/بوت.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `✦┇اهــلًا للمـساعـدة اكــتب الـاوامـر┇✓`;
+    let message = `✦*┇ اهــلًا للمـساعـدة اكــتب الـاوامـر ┇*✓`;
 
     conn.sendFile(m.chat, 'https://telegra.ph/file/ea51c795740b96b5a5f17.jpg', 'image.jpg', message, m);
 };
