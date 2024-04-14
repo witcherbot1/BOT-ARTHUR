79367725dc70efd3120b74edb0d20102a4fbe207
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-14 21:43:30 +0300

    Update اطفاء-banchat.js

--- plugins/اطفاء-banchat.js
+++ plugins/اطفاء-banchat.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('*تـــم اطـفـاء الـبـوت فــي هـذا الـجروب*')
+    m.reply('*✦┃تـــم اطـفـاء الـبـوت فــي هـذا الـجروب┃✦*')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
