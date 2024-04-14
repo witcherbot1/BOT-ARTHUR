ddba9a73ed77bf746c4477031a36bf93c6dc7276
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-14 17:14:52 +0300

    Update ايديت-2.js

--- plugins/ايديت-2.js
+++ plugins/ايديت-2.js
@@ -1,63 +1,109 @@
-import fetch from "node-fetch";
-import uploadImage from "../lib/uploadImage.js";
-let handler = async (m, { conn, usedPrefix, command }) => {
-conn.unblur_high = conn.unblur_high ? conn.unblur_high : {}
-if (m.sender in conn.unblur_high) throw "*تحويل صورة قديمة الى صورة ذات جودة عالية*"
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || q.mediaType || ""
-if (!mime) throw `*ارسل صورة مصحوبة بكلمة ${usedPrefix + command}*`
-if (!/image\/(jpe?g|png)/.test(mime)) throw `*[❗] 𝙴𝙻 𝙵𝙾𝚁𝙼𝙰𝚃𝙾 𝙳𝙴𝙻 𝙰𝚁𝙲𝙷𝙸𝚅𝙾 (${mime}) 𝙽𝙾 𝙴𝚂 𝙲𝙾𝙼𝙿𝙰𝚁𝚃𝙸𝙱𝙻𝙴, 𝙴𝙽𝚅𝙸𝙰 𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽𝙰 𝙵𝙾𝚃𝙾*`
-else conn.unblur_high[m.sender] = true
-m.reply('*جاري معالجة الصورة*')
-let img = await q.download?.()
-let upld = await uploadImage(img)
-let img2
-try {
-img2 = await fetch(`https://api.itsrose.site/image/unblur?url=${upld}&apikey=${global.itsrose}`)
-let image = await img2.arrayBuffer()
-conn.sendFile(m.chat, image, null, '', m)
-} catch {
-m.reply("*error*");
-} finally {
-delete conn.unblur_high[m.sender]
-}}
-handler.help = ["remini", "hd", "enhance"]
-handler.tags = ["ai", "tools"]
-handler.command = ["remini", "hd", "جوده"]
-export default handler
+const dir = [
+'https://telegra.ph/file/d6269a1f7f2bf94a406df.mp4',  'https://telegra.ph/file/8034305ce5330ebc11a99.mp4',  'https://telegra.ph/file/5c70fbac268fb54ff847e.mp4',  'https://telegra.ph/file/f2a6bec5b7635364d6768.mp4',  'https://telegra.ph/file/d7f5799da8e64b9aff5aa.mp4',  'https://telegra.ph/file/261100ff5fe590b08e35d.mp4', 'https://telegra.ph/file/6214d68e0da156ef8e54a.mp4',
+'https://telegra.ph/file/960bece94cac521c5fd68.mp4',
+'https://telegra.ph/file/759c10b0e0a1605ae9716.mp4',
+'https://telegra.ph/file/1d4d8d50e19929f4870f6.mp4',
+'https://telegra.ph/file/07941ba2e117fea621e5d.mp4',
+'https://telegra.ph/file/5fb988f765cd747cde120.mp4',
+'https://telegra.ph/file/5ddb8eec9a8d4883dde9c.mp4',
+'https://telegra.ph/file/bc4cafca3f25376e7cb2e.mp4',
+'https://telegra.ph/file/2243a5e4d437536f7dc8b.mp4',
+'https://telegra.ph/file/65ffa16e5eaee2715e713.mp4',
+'https://telegra.ph/file/d1ae62ec93fdfbfb44f11.mp4',
+'https://telegra.ph/file/539d50387958f5d58e76c.mp4',
+'https://telegra.ph/file/aa6c00ce3e0e07d6775dd.mp4',
+'https://telegra.ph/file/6a8818cad48e79495390a.mp4',
+'https://telegra.ph/fil e/141b6f6cda402a8de3c3e.mp4',
+'https://telegra.ph/file/1180e55bd2cf29924c8d2.mp4',
+'https://telegra.ph/file/38bde47b6402f2bfb06aa.mp4', 
+'https://telegra.ph/file/32344686758198df923a1.mp4',
+'https://telegra.ph/file/38bc86129afa59776ec41.mp4',
+'https://telegra.ph/file/db3c8672cde1ca5e6552a.mp4',
+'https://telegra.ph/file/830101289dc6bd39811bb.mp4',
+'https://telegra.ph/file/822c53d16b73d2dde32d3.mp4',
+'https://telegra.ph/file/886982e1eebd12cd53395.mp4',
+'https://telegra.ph/file/7d2cdc5219bf3a551aa63.mp4',
+'https://telegra.ph/file/0ac041f0b5a3afc29e017.mp4',
+'https://telegra.ph/file/ea5e5e24b7fb66974473d.mp4',
+'https://telegra.ph/file/92c7e1232a8bf6363e4f9.mp4',
+'https://telegra.ph/file/961d4145ccc3e7f1c01ad.mp4',
+'https://telegra.ph/file/7678a0f51d614c4c16f13.mp4',
+'https://telegra.ph/file/0bd52734057a8a8edb95e.mp4',
+'https://telegra.ph/file/02ca204f4e6a35f857574.mp4',
+'https://telegra.ph/file/fdee2c0ae99a871c879f2.mp4',
+'https://telegra.ph/file/c390eb0c4663ec4c58e9f.mp4',
+'https://telegra.ph/file/e53d1ca92142e93a9d3b8.mp4',
+'https://telegra.ph/file/0584a114a36c10db6a611.mp4',
+'https://telegra.ph/file/6b04390e25c21b28b482c.mp4',
+'https://telegra.ph/file/de7eda58ae3b945410c19.mp4',
+'https://telegra.ph/file/8be0819b1a9ccafd74653.mp4',
+'https://telegra.ph/file/3affb77764c72e541c9c7.mp4',
+'https://telegra.ph/file/75299bee060e8342daf3c.mp4',
+'https://telegra.ph/file/a64a814e561242128d11e.mp4',
+'https://telegra.ph/file/28db308ee36082e5bb9fd.mp4',
+'https://telegra.ph/file/540faab16cbdcb568b241.mp4',
+'https://telegra.ph/file/77d4d5e03015da8411f71.mp4',
+'https://telegra.ph/file/cc672124f0b8afe2dd020.mp4',
+'https://telegra.ph/file/638a8885025af228f6de6.mp4',
+'https://telegra.ph/file/e108c60492a2dc2b9d48f.mp4',
+'https://telegra.ph/file/2105fb2cbfee931204e12.mp4',
+'https://telegra.ph/file/af4397b8abac12bd81c76.mp4',
+'https://telegra.ph/file/703dc161569560d4b9533.mp4',
+'https://telegra.ph/file/381cbc23153d4a979300c.mp4',
+'https://telegra.ph/file/6fa34083e9d0605de39ca.mp4',
+'https://telegra.ph/file/f5fa34862faba7c29875a.mp4',
+'https://telegra.ph/file/9419ab2ddbb1f62193e3f.mp4',
+'https://telegra.ph/file/5d3493f1eaff7e69d578d.mp4',
+'https://telegra.ph/file/a821c3b25bbfa406a4da0.mp4',
+'https://telegra.ph/file/9d33ae0a2830d03c820c2.mp4',
+'https://telegra.ph/file/a8cb1c29be302dc938dbe.mp4',
+'https://telegra.ph/file/a907cc16f4ae28171d0dd.mp4',
+'https://telegra.ph/file/f3ac58e739933a6c3654e.mp4',
+'https://telegra.ph/file/c5e36f2a11976f3c9b1f9.mp4',
+'https://telegra.ph/file/3aba3b86aced4c00e3b05.mp4',
+    'https://telegra.ph/file/03cc6997426e99c687e6a.mp4',
+    'https://telegra.ph/file/5a9f45e1842da8a02510d.mp4',
+    'https://i.imgur.com/OAbYGlz.mp4',
+    'https://i.imgur.com/Pad6zvr.mp4',
+    'https://telegra.ph/file/5df925bf07f7b35c566a9.mp4',
+    'https://telegra.ph/file/296446e7bda8435cff317.mp4',
+    'https://telegra.ph/file/fd5af468337aa6b983c63.mp4',
+    'https://telegra.ph/file/ab1e1700e30be96ad23ae.mp4',
+    'https://i.imgur.com/NILGHXC.mp4',
+    'https://i.imgur.com/dztvsNW.mp4',
+    'https://telegra.ph/file/b16bae2461e7e8f708714.mp4',
+    'https://telegra.ph/file/9e75b87f61514b2ac1891.mp4',
+    'https://telegra.ph/file/a0baddc3b867f94343e39.mp4',
+    'https://telegra.ph/file/e27c0b4190d2d107fe92a.mp4',
+    'https://telegra.ph/file/dc0c1f36245ea087e179c.mp4',
+    'https://telegra.ph/file/2b961f5f333cb8e0a71b5.mp4',
+    'https://telegra.ph/file/034fbe32c484b4c1fb879.mp4',
+    'https://telegra.ph/file/22d52f7feb7f6c8cf48ab.mp4',
+    'https://telegra.ph/file/00bb241d3408dc711f4c7.mp4',
+    'https://telegra.ph/file/0f519faa00b985a225097.mp4',
+    'https://telegra.ph/file/a8ff62adaac7b36e020ef.mp4',
+    'https://telegra.ph/file/491d97b8e5aad56ce8a62.mp4',
+    'https://telegra.ph/file/bb6f549a4aa04370656af.mp4',
+    'https://telegra.ph/file/565c34124ead8a15497b2.mp4',
+    'https://telegra.ph/file/9c51f97917ed5fd88a6f6.mp4',
+    'https://telegra.ph/file/900377152f73574238d51.mp4',
+    'https://telegra.ph/file/e7fb1642655952aea07ec.mp4',
+    'https://telegra.ph/file/11bd0474398abde00d99c.mp4',
+    'https://telegra.ph/file/85f5a1b00e49f9352461e.mp4',
+    'https://telegra.ph/file/f41d31cf3c95623ae311e.mp4',
+    'https://i.imgur.com/yPcceZE.mp4',
+    'https://telegra.ph/file/996aaeedce16a8b5a7964.mp4',
+    'https://telegra.ph/file/3ce27541be63bdad47bdd.mp4',
+    'https://telegra.ph/file/b36ef01fc267e4f331caf.mp4',
+    'https://i.imgur.com/3DDgCzb.mp4',
+    'https://i.imgur.com/8QDdUHo.mp4',
+];
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '', m)
+  await conn.sendMessage(m.chat, { react: { text: '📹', key: m.key } })
+}
+handler.help = ['dado']
+handler.tags = ['game']
+handler.command = ['2ايديت2', 'اديت'] 
 
-/*import uploadImage from '../lib/uploadImage.js'
-import deepai from 'deepai'
-import fetch from 'node-fetch'
-import FormData from 'form-data'
-deepai.setApiKey('04f02780-e0bd-44c1-afa2-14cf5a78948c')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || q.mediaType || ''
-if (/image/g.test(mime) && !/webp/g.test(mime)) {
-try {
-let img = await q.download?.()
-let out = await uploadImage(img)
-var resp = await deepai.callStandardApi("waifu2x", { image: out })
-let w2x1 = resp['output_url']
-var resep = await deepai.callStandardApi("waifu2x", { image: w2x1 })
-let w2x2 = resep['output_url']
-var resup = await   deepai.callStandardApi("torch-srgan", { image: w2x2 })
-await conn.sendFile(m.chat, resup['output_url'], 'error.png', '', m)
-} catch {
-await m.reply(global.wait)
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || q.mediaType || ''
-if (!mime) throw '*[❗] 𝙴𝙽𝚅𝙸𝙰 𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽𝙰 𝙵𝙾𝚃𝙾*'
-if (!/image\/(jpe?g|png)/.test(mime)) throw `*[❗] 𝙴𝙻 𝙵𝙾𝚁𝙼𝙰𝚃𝙾 𝙳𝙴𝙻 𝙰𝚁𝙲𝙷𝙸𝚅𝙾 (${mime}) 𝙽𝙾 𝙴𝚂 𝙲𝙾𝙼𝙿𝙰𝚁𝚃𝙸𝙱𝙻𝙴, 𝙴𝙽𝚅𝙸𝙰 𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽𝙰 𝙵𝙾𝚃𝙾*`
-let img = await q.download()
-let body = new FormData
-body.append('image', img, 'image')
-let res = await fetch('http://max-image-resolution-enhancer.codait-prod-41208c73af8fca213512856c7a09db52-0000.us-east.containers.appdomain.cloud/model/predict', { method: 'POST', body })
-if (res.status !== 200) throw await res.json()
-await conn.sendFile(m.chat, await res.buffer(), 'error.jpg', '', m)}} else {
-m.reply(`*[❗] 𝙴𝙽𝚅𝙸𝙴 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝙲𝙾𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`)}}
-handler.help = ['hd', 'enhance']
-handler.tags = ['tools']
-handler.command = /^(hd|enhance)$/i
-export default handler*/
+export default handler
