7008c8577c8cc07f0be7a3fbeebae63a9975bbeb
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-14 18:15:37 +0300

    Update امر11.js

--- plugins/امر11.js
+++ plugins/امر11.js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let videoUrl = 'https://telegra.ph/file/3ce3f3a96e76b34a6d94e.jpg';
+let videoUrl = 'https://telegra.ph/file/56034655fe12b9fa53a9d.jpg';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
@@ -69,7 +69,7 @@ conn.sendMessage(m.chat, {
 }; 
 handler.help = ['main']
 handler.tags = ['ggsy']
-handler.command = ['11امر'] 
+handler.command = ['امر11'] 
 
 export default handler
 function clockString(ms) {
