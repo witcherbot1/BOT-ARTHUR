6e95d6997a9da4c4ad29614bc9416ad61fc24eb2
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-15 20:54:33 +0300

    Add files via upload

--- plugins/gp-delwarn.js
+++ plugins/gp-delwarn.js
@@ -3,24 +3,24 @@ let handler = async (m, { conn, args, groupMetadata}) => {
         let who
         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
         else who = m.chat
-        if (!who) throw `*✦┇مــنشـن الـشـخص ┇✦*`
-        if (!(who in global.db.data.users)) throw `*✦┇المستخدم غير موجود في قاعدة البيانات الخاصة بي┇✦*`
+        if (!who) throw `✳️ Tag or mention someone`
+        if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
        let warn = global.db.data.users[who].warn
        if (warn > 0) {
          global.db.data.users[who].warn -= 1
-         m.reply(` *❮⚠️┇ حذف الانذار ❯*
+         m.reply(`⚠️ *DELWARN*
          
-〄┇الانذار : *-1✓*
-*〄┇ عدد الانذارات الحالي: ${warn - 1}┇〄*`)
-         m.reply(`✓┇ قام أحد المسؤولين بتخفيض تحذيره ، والآن لديك┇〄 *${warn - 1}*`, who)
+▢ Warns: *-1*
+▢ Warns total: *${warn - 1}*`)
+         m.reply(`✳️ An admin lowered their warning, now you have *${warn - 1}*`, who)
          } else if (warn == 0) {
-            m.reply('*〄┇المستخدم ليس لديه انذارات┇〄*')
+            m.reply('✳️ The user has no warning')
         }
 
 }
 handler.help = ['delwarn @user']
 handler.tags = ['group']
-handler.command = ['حذف_انذار', 'حذف_تحذير'] 
+handler.command = ['delwarn', 'unwarn'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
