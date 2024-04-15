04367d410595eb150dc1854d48a9041e0b043e1d
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-15 03:48:12 +0300

    Update gp-delwarn.js

--- plugins/gp-delwarn.js
+++ plugins/gp-delwarn.js
@@ -3,18 +3,18 @@ let handler = async (m, { conn, args, groupMetadata}) => {
         let who
         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
         else who = m.chat
-        if (!who) throw `*مــنشـن الـشـخص !*`
-        if (!(who in global.db.data.users)) throw `*المستخدم غير موجود في قاعدة البيانات الخاصة بي*`
+        if (!who) throw `*✦┇مــنشـن الـشـخص ┇✦*`
+        if (!(who in global.db.data.users)) throw `*✦┇المستخدم غير موجود في قاعدة البيانات الخاصة بي┇✦*`
        let warn = global.db.data.users[who].warn
        if (warn > 0) {
          global.db.data.users[who].warn -= 1
          m.reply(`⚠️ *حذف الانذار*
          
-▢الانذار : *-1*
-▢ عدد الانذارات الحالي: *${warn - 1}*`)
-         m.reply(`✳️ قام أحد المسؤولين بتخفيض تحذيره ، والآن لديك *${warn - 1}*`, who)
+✓الانذار : *-1┇〄*
+*〄┇ عدد الانذارات الحالي: *${warn - 1}*┇〄`)
+         m.reply(`┇✓ قام أحد المسؤولين بتخفيض تحذيره ، والآن لديك┇〄 *${warn - 1}*`, who)
          } else if (warn == 0) {
-            m.reply('*المستخدم ليس لديه انذارات*')
+            m.reply('*〄┇المستخدم ليس لديه انذارات┇〄*')
         }
 
 }
