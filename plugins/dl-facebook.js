c7565da328a4c79c2047c558cab8307bf626c75e
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-19 16:49:56 +0300

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -14,7 +14,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
   try {
     const result = await fg.fbdl(args[0]);
     const tex = `
-*❒⊱──┇ بــــ ناتسو ــــوت ┇──⊰❒*
+*❒⊱──┇ بــوت ┇──⊰❒*
 
 ↳ *عـنوان الــفيـديو:* ${result.title}
 
