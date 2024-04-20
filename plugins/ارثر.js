d5eff2a4498a674898fa93c21b77ecce61b5f750
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-20 21:23:20 +0300

    Update ارثر.js

--- plugins/ارثر.js
+++ plugins/ارثر.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
     conn.sendFile(m.chat, 'https://telegra.ph/file/48d5818432463d9e3f2bd.jpg', 'image.jpg', message, m);
 };
 
-handler.customPrefix = /^(امر 7|امر 2|امر 1|امر|امر 3|امر 4|امر 5|امر 6)$/i;
+handler.customPrefix = /^(امر 7|امر 2|.امر 1|.امر|.امر 3|.امر 4|.امر 5|.امر 6.)$/i;
 handler.command = new RegExp;
 
 export default handler;
