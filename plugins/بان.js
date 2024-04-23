6b7c2aeaa96a81803ed1479807b5bca5a84afbfb
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-23 15:27:07 +0300

    Update بان.js

--- plugins/بان.js
+++ plugins/بان.js
@@ -5,10 +5,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) throw `□ منشن الشخص`
+    if (!who) throw `*✤┇منشن الشخص┇✤*`
     let users = global.db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `@${who.split`@`[0]} لن تستطيع استخدام اوامر بعد الان !`, m, { mentions: [who] })
+    conn.reply(m.chat, `*✦┇@${who.split`@`[0]} لن تستطيع استخدام اوامر بعد الان !┇✦*`, m, { mentions: [who] })
 }
 handler.help = ['ban @user']
 handler.tags = ['owner']
