9d20b7b60c44a6158dde8f43c8c7542c851f5e08
12345eren <159772923+12345eren@users.noreply.github.com>
2024-05-01 04:17:47 +0300

    Update ارثر.js

--- plugins/ارثر.js
+++ plugins/ارثر.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
     let message = `*⨂┇ الـامــر خـطـأ ســوي نـقـطـة وبـدون فـواصـل ┇〄*`;
 
-    conn.sendFile(m.chat, 'https://telegra.ph/file/48d5818432463d9e3f2bd.jpg', 'image.jpg', message, m);
+    conn.sendFile(m.chat, 'https://telegra.ph/file/e4fb5b98cb6ab7255c722.jpg', 'image.jpg', message, m);
 };
 
 handler.customPrefix = /^(امر 7|امر 2|.امر 1|.امر|.امر 3|.امر 4|.امر 5|.امر 6.|امر)$/i;
