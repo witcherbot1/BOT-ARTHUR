dd2bed7ee9e0870c44ebf56fc6c283d737e4a8f8
12345eren <159772923+12345eren@users.noreply.github.com>
2024-05-03 01:51:22 +0300

    Update تغيرالخلفية.js

--- plugins/تغيرالخلفية.js
+++ plugins/تغيرالخلفية.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let img = await q.download()
   if (!img) throw '*[❗] رد علی صوره فقط*'
   await conn.updateProfilePicture(m.chat, img).then(_ => m.reply('⚘*┇تم تغير الصوره بنجاح┇✦*'))
-  } else throw '*↞ رد علی الصورة اللتي ترید به تغير افتار المجموعه┇✦'}
+  } else throw '*↞ رد علی الصورة اللتي ترید به تغير افتار المجموعه┇✦*'}
   handler.command = /^(تغيرافتار|تغيرالافتاره|تغير-خلفيه|تغيرالصوره)$/i
   handler.group = true
   handler.admin = true
