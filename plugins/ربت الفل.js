17f154a797de660cbe613a797352313c78979819
12345eren <159772923+12345eren@users.noreply.github.com>
2024-05-03 01:12:57 +0300

    Update ربت الفل.js

--- plugins/ربت الفل.js
+++ plugins/ربت الفل.js
@@ -9,9 +9,9 @@ global.rpg = {
       { name: "جندي", level: 0 }, { name: "مبتدئ", level: 4 }, 
       { name: "محارب", level: 8 }, { name: "مغوار", level: 12 }, 
       { name: "فارس", level: 16 }, { name: "مغامر", level: 20 }, 
-      { name: "بطل", level: 24 }, { name: "محنك", level: 28 }, 
+      { name: "بطل", level: 24 }, { name: "نينجا", level: 28 }, 
       { name: "ساحر", level: 32 }, { name: "حكيم", level: 36 },
-      { name: "دوق", level: 48 }, { name: "الساحر الأسود", level: 52 }, 
+      { name: "نخبة", level: 48 }, { name: "الساحر الأسود", level: 52 }, 
       { name: "صياد", level: 56 }, { name: "الملك", level: 60 }, 
       { name: "ملك", level: 100 }
     ];
