cd36bbf190c375110873985798703264e734f3c0
12345eren <159772923+12345eren@users.noreply.github.com>
2024-05-09 14:36:23 +0300

    Update بان.js

--- plugins/بان.js
+++ plugins/بان.js
@@ -1,18 +1,12 @@
-//import db from '../lib/database.js'
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-   let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-    else who = m.chat
-    let user = global.db.data.users[who]
-    if (!who) throw `*✤┇منشن الشخص┇✤*`
-    let users = global.db.data.users
-    users[who].banned = true
-    conn.reply(m.chat, `*✦┇@${who.split`@`[0]} لن تستطيع استخدام اوامر بعد الان !┇✦*`, m, { mentions: [who] })
-}
-handler.help = ['ban @user']
-handler.tags = ['owner']
+let handler = async (m, { conn, participants, usedPrefix, command }) => {
+let BANtext = `✦┇منشن او ريبلاي للشخص المراد حظره\n\n*—◉ مثال :*\n*${usedPrefix + command} @${global.suittag}┇✦*`
+if (!m.mentionedJid[0] && !m.quoted) return m.reply(BANtext, m.chat, { mentions: conn.parseMention(BANtext)})
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+else who = m.chat
+let users = global.db.data.users
+users[who].banned = true
+m.reply('*✦┇تم حظر المستخدم بنجاح*\n*—◉ لن يتمكن المستخدم من استخدام البوت حتي يتم الغاء الحظر┇✦*')    }
 handler.command = /^بان$/i
 handler.rowner = true
-
 export default handler
