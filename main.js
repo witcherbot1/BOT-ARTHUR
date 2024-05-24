ab64a1e405a098c286982fb2c165297f816fde61
12345eren <159772923+12345eren@users.noreply.github.com>
2024-05-25 02:59:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,8 @@ const __dirname = global.__dirname(import.meta.url);
 global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
 global.prefix = new RegExp('^[' + (opts['prefix'] || '*/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-.@').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']');
 
-global.db = new Low(/https?:\/\//.test(opts['db'] || '') ? new cloudDBAdapter(opts['db']) : new JSONFile(`${opts._[0] ? opts._[0] + '_' : ''}database.json`));
+global.db = new Low(/https?:\/\//.test(opts['db'] || '') ? new cloudDBAdapter(opts['db']) : new JSONFile(`${opts._[0] ? opts._[0] + '_' : ''}database.json`)
+);
 
 
 import firebaseAdmin from 'firebase-admin';
@@ -93,6 +94,7 @@ async function saveDataToFirebase() {
 
 // تنفيذ الكود كل دقيقة
 setInterval(saveDataToFirebase, 60000);
+
 
 global.DATABASE = global.db; 
 global.loadDatabase = async function loadDatabase() {
